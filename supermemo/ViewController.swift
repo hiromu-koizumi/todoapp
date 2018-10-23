2886c61bf623e42262dbd0830984f8681529deaa
hiromu-koizumi <abn21029@gmail.com>
2018-10-23 22:13:16 +0900

    Initial Commit

---
+++ supermemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  supermemo
+//
+//  Created by 小泉大夢 on 2018/10/23.
+//  Copyright © 2018 小泉大夢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
